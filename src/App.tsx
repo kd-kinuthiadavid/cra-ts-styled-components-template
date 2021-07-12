27811e15c65500e5dc78fe6ec0f3d2e0a06c9343
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2021-07-12 19:55:56 +0300

    config - configure styled-components to work with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./theme";
+
+const StyledApp = styled.div`
+  color: ${props => props.theme.colors.secondary};
+  background-color: black;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={defaultTheme} >
+      <StyledApp className="App">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus est voluptatum quasi quos, illo sequi reiciendis obcaecati commodi nisi, minima nihil voluptas nemo voluptatem distinctio ab maiores deleniti modi tempora.
+        Eum velit nam accusantium voluptate! Ea laudantium tempora at ducimus! Porro, odio alias libero nostrum vero quo. Facilis porro fugit culpa laudantium doloribus molestias ex, iusto a minima maxime tempora!
+        Sed, nam at, nemo harum deserunt corrupti nihil natus perferendis saepe fugit aperiam cum culpa, pariatur atque. Quo ut laborum qui possimus optio iusto illum veritatis quos, natus sunt nostrum?
+        Laboriosam a nam corporis ullam asperiores vel dicta expedita laborum nostrum, dolores nemo deleniti ut fugit voluptatum quo nesciunt minus quae non rerum architecto, quas voluptatem beatae possimus! Consequuntur, at!
+        Quod officiis cupiditate doloribus qui eos maiores nobis nam consectetur optio labore, perspiciatis voluptas. Consequuntur recusanvoluptas culpa facilis cum at quis qui.
+        Laboriosam temporibus aspernatur earum, nemo debitis corrupti? Exercitationem laudantium soluta dignissimos magnam eveniet quis a unde laborum aliquam? Voluptatem eligendi voluptatum amet dolorem! Maxime rem, inventore doloremque aut iste perferendis!
+        Fugit unde quidem rerum fuga enim maxime corporis adipisci! Aspernatur ab perspiciatis laboriosam nihil sint veritatis temporibus vel similique quae officiis qui at obcaecati, architecto ipsa quidem repudiandae cupiditate error.
+        Repudiandae ratione, enim minus quidem quam recusandae soluta iure deserunt ipsum explicabo nemo quibusdam sunt delectus suscipit. Nulla voluptates impedit natus, tempora molestias aspernatur doloribus beatae expedita. Adipisci, sunt quibusdam.
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+const defaultTheme: DefaultTheme = {
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+};
+
+export { defaultTheme };
